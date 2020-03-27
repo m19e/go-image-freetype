8c85e31247d40259452a14dd6572672effd62852
m19e <nexus.me7290@gmail.com>
2020-03-28 06:16:27 +0900

    テキストを画像の載せました！！！！！

---
+++ main.go
@@ -0,0 +1,75 @@
+package main // import "type"
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/png"
+	"io/ioutil"
+	"os"
+
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+)
+
+func main() {
+	ftBin, err := ioutil.ReadFile("static/Koruri-Bold.ttf")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	ft, err := truetype.Parse(ftBin)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	opt := truetype.Options{
+		Size:              90,
+		DPI:               0,
+		Hinting:           0,
+		GlyphCacheEntries: 0,
+		SubPixelsX:        0,
+		SubPixelsY:        0,
+	}
+
+	imageWidth := 100
+	imageHeight := 100
+	textTopMargin := 90
+	text := "ほ"
+
+	img := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
+
+	face := truetype.NewFace(ft, &opt)
+
+	dr := &font.Drawer{
+		Dst:  img,
+		Src:  image.Black,
+		Face: face,
+		Dot:  fixed.Point26_6{},
+	}
+
+	dr.Dot.X = (fixed.I(imageWidth) - dr.MeasureString(text)) / 2
+	dr.Dot.Y = fixed.I(textTopMargin)
+
+	dr.DrawString(text)
+
+	buf := &bytes.Buffer{}
+	err = png.Encode(buf, img)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	file, err := os.Create(`test.png`)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	file.Write(buf.Bytes())
+}
